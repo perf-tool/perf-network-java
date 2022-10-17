34e625cd18eb511a1efae855b7e6819a844d73c8
Lico-Tom <583566138@qq.com>
2022-10-17 17:44:29 +0800

    redis and mongo trace reporter impl.

--- src/main/java/com/perftool/network/tcp/ServerHandler.java
+++ src/main/java/com/perftool/network/tcp/ServerHandler.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -19,17 +19,65 @@
 
 package com.perftool.network.tcp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.perftool.network.constant.PerfnConst;
+import com.perftool.network.module.TcpMessage;
+import com.perftool.network.trace.TraceBean;
+import com.perftool.network.trace.TraceReporter;
+import com.perftool.network.trace.module.SpanInfo;
+import com.perftool.network.trace.mongo.MongoClientImpl;
+import com.perftool.network.trace.redis.RedisClientImpl;
+import com.perftool.network.util.EnvUtil;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
 
 @Slf4j
 @ChannelHandler.Sharable
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
+    private static TraceReporter traceReporter = null;
+
+    private static String traceType = EnvUtil.getString("TRACE_TYPE", "DUMMY");
+
+    static {
+        switch (traceType) {
+            case "MONGO" -> traceReporter = new MongoClientImpl();
+            case "REDIS" -> traceReporter = new RedisClientImpl();
+        }
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof ByteBuf) {
+            ByteBuf in = (ByteBuf) msg;
+            try {
+                if (in.readableBytes() > 0) {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    TcpMessage tcpMessage = objectMapper.readValue(in.toString(CharsetUtil.UTF_8), TcpMessage.class);
+                    if (Optional.ofNullable(traceReporter).isPresent()) {
+                        TraceBean traceBean = new TraceBean();
+                        long createTime = System.currentTimeMillis();
+                        traceBean.setCreateTime(createTime);
+                        SpanInfo spanInfo = new SpanInfo();
+                        spanInfo.setReceiveTime(createTime);
+                        traceBean.setSpanId(spanInfo);
+                        traceBean.setTraceId(tcpMessage.getTcpHeader().getTraceId());
+                        traceReporter.reportTrace(traceBean, PerfnConst.COMM_TYPE_SERVER);
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                ReferenceCountUtil.release(in);
+            }
+        }
         ctx.write(msg);
     }
 
---
+++ src/main/java/com/perftool/network/trace/mongo/MongoClientImpl.java
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.perftool.network.trace.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.perftool.network.trace.TraceBean;
+import com.perftool.network.trace.TraceReporter;
+import com.perftool.network.util.ConfigUtil;
+import org.bson.Document;
+
+import java.util.ArrayList;
+
+public class MongoClientImpl implements TraceReporter {
+
+    private MongoConfig config;
+
+    public MongoClientImpl() {
+        this.config = ConfigUtil.getMongoConfig();
+    }
+
+    public MongoDatabase createMongoDatabase() {
+        if ("".equalsIgnoreCase(config.mongodbPassword)
+                || "".equalsIgnoreCase(config.mongodbUsername)) {
+            MongoClient mongoClient = new MongoClient(config.mongodbHost, config.mongodbPort);
+            return mongoClient.getDatabase(config.mongodbDatabaseName);
+        } else {
+            ServerAddress addr = new ServerAddress(config.mongodbHost, config.mongodbPort);
+            ArrayList<ServerAddress> addresses = new ArrayList<>();
+            addresses.add(addr);
+            MongoCredential credential = MongoCredential.createScramSha1Credential(config.mongodbUsername,
+                    config.mongodbDatabaseName,
+                    config.mongodbPassword.toCharArray());
+            MongoClientOptions options = MongoClientOptions.builder().build();
+            MongoClient mongoClient = new MongoClient(addresses, credential, options);
+            return mongoClient.getDatabase(config.mongodbDatabaseName);
+        }
+    }
+
+    @Override
+    public void reportTrace(TraceBean traceBean, String commType) {
+        MongoDatabase database = createMongoDatabase();
+        initCollection(database);
+        MongoCollection<Document> collection = database.getCollection(config.mongodbCollectionName + "_" + commType);
+        Document document = new Document("traceId", traceBean.getTraceId())
+                .append("createTime", traceBean.getCreateTime())
+                .append("spanId", traceBean.getSpanId());
+        collection.insertOne(document);
+    }
+
+    private void initCollection(MongoDatabase database) {
+        database.createCollection(config.mongodbCollectionName);
+    }
+
+}
